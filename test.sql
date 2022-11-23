3f7e7e9e6b5dca41d867c5d009d10e046158192c
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-11-23 09:44:46 +0200

    Delete test.sql

--- test.sql
+++
@@ -1,7 +0,0 @@
-Saying lot's of thingss an language or something*
-
-dddddddddddddddddddddddddddddddddddddddddddd
-fffffffffffffffffffffff
-gggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-SLECT SMLHT 'stirng' = 15
-fdkf
